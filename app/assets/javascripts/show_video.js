242d08424959dbafdf96008b0a4da5ed50af5e3f
Jin Park <jin+github@jinpark.net>
2014-12-10 17:50:32 +0900

    add autoplay cookie switching button

--- app/assets/javascripts/show_video.js
+++ app/assets/javascripts/show_video.js
@@ -1,9 +1,19 @@
 $(document).ready(function(){
 	if ($.cookie('autoplay') == 'true') {
+		$('.autoplay-btn').addClass('active');
 		$('video')[0].play();
 		$('video').bind("ended", function(){
 			window.location = $('.next-link').attr('href');
 		})
-	}
-
+	} 
+	$('.autoplay-btn').on('click', function(){
+		if ($.cookie('autoplay') == 'true') {
+			$.cookie('autoplay', 'false');
+			$('.autoplay-btn').removeClass('active');
+			$('video')[0].play();
+		} else {
+			$.cookie('autoplay', 'true');
+			$('.autoplay-btn').addClass('active');
+		}
+	})
 })
