9a2a62f225786e7bb4d6df4bb305a1198a810aa7
Jin Park <jin+github@jinpark.net>
2015-01-29 17:10:28 +0900

    cleanup show_video.js code

--- app/assets/javascripts/show_video.js
+++ app/assets/javascripts/show_video.js
@@ -1,39 +1,23 @@
 $(document).ready(function(){
-	if ($.cookie('autoplay') == 'true') {
-		$('.autoplay-btn').addClass('active');
-		$('video')[0].play();
-		$('video').bind("ended", function(){
-			window.location = $('.next-link').attr('href');
-		})
-	} 
-	$('.autoplay-btn').on('click', function(){
-		if ($.cookie('autoplay') == 'true') {
-			$.cookie('autoplay', 'false');
-			$('.autoplay-btn').removeClass('active');
-			$('video')[0].play();
-		} else {
-			$.cookie('autoplay', 'true');
-			$('.autoplay-btn').addClass('active');
-		}
-	});
-	videojs('main-video').ready(function() {
-	  this.hotkeys({
-	    volumeStep: 0.1,
-	    seekStep: 5,
-	    enableMute: true,
-	    enableFullscreen: true
-	  });
-	  this.on('play', function(){
-	  	$(this).focus();
-	  });
-	  this.on('pause', function(){
-	  	$(this).focus();
-	  })
-	});
-	$('#main-video').focus();
-
 	videojs('main-video').ready(function() {
 		var video = this;
+		// setup hotkeys
+		this.hotkeys({
+	    	volumeStep: 0.1,
+	    	seekStep: 5,
+	    	enableMute: true,
+	    	enableFullscreen: true
+		});
+		// force focus on video so hotkeys work
+		$(this).focus();
+		this.on('play', function(){
+	  		$(this).focus();
+	  	});
+	  	this.on('pause', function(){
+	  		$(this).focus();
+	  	})
+
+	  	// screenshot function
 		$('#new_screenshot').on('ajax:before', function() {
 			var canvas = document.createElement('canvas');
 			canvas.width = video.I.videoWidth * 2;
@@ -44,13 +28,32 @@ $(document).ready(function(){
 			$(this).find('#screenshot_data_uri').val(dataURI);
 			$(this).find('#screenshot_time').val(video.currentTime());
 		})
+		// open new window with screenshot
 		$('#new_screenshot').on('ajax:success',function (event, data, status, xhr) {
 			window.open(data.attachment.attachment.url, '_blank');
 		})
+		// clear form to save on dom
 		$('#new_screenshot').on('ajax:complete',function (event, data, status, xhr) {
 			$(this).find('#screenshot_data_uri').val('');
 		})
 
-
+		// autoplay stuff
+		if ($.cookie('autoplay') == 'true') {
+			$('.autoplay-btn').addClass('active');
+			$('video')[0].play();
+			$('video').bind("ended", function(){
+				window.location = $('.next-link').attr('href');
+			})
+		} 
+		$('.autoplay-btn').on('click', function(){
+			if ($.cookie('autoplay') == 'true') {
+				$.cookie('autoplay', 'false');
+				$('.autoplay-btn').removeClass('active');
+				$('video')[0].play();
+			} else {
+				$.cookie('autoplay', 'true');
+				$('.autoplay-btn').addClass('active');
+			}
+		});
 	})
 })
