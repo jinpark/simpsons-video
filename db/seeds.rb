bb6891e3ba18b9e8f699d214d126a401ea759643
Jin Park <jin+github@jinpark.net>
2015-07-02 18:20:36 +0900

    change seeds, change database yml, docker compose

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 Video.create!([
-  {title: "Treehouse of Horror VI", season: 7, episode_number: 6, thumbnail: "/videos/thumbnail/s07e06.png", path: "/videos/", filename: "s07e06.mp4"},
-  {title: "The Day the Violence Died", season: 7, episode_number: 18, thumbnail: "/videos/thumbnail/s07e18.png", path: "/videos/", filename: "s07e18.mp4"}
+  {title: "The Day the Violence Died", season: 7, episode_number: 18, thumbnail: "/videos/thumbnail/s07e18.png", path: "/videos/", filename: "s07e18.mp4", slug: "the-day-the-violence-died", subtitle_path: "/subtitles/7/", subtitle_filename: "7x18.srt"},
+  {title: "Blazed and Confused", season: 26, episode_number: 7, thumbnail: "/videos/thumbnail/s26e07.png", path: "/videos/", filename: "s26e07.mp4", slug: "blazed-and-confused", subtitle_path: "/subtitles/26/", subtitle_filename: "26x07.srt"},
+  {title: "Treehouse of Horror VI", season: 7, episode_number: 6, thumbnail: "/videos/thumbnail/s07e06.png", path: "/videos/", filename: "s07e06.mp4", slug: "treehouse-of-horror-vi", subtitle_path: "/subtitles/7/", subtitle_filename: "7x06.srt"}
 ])
